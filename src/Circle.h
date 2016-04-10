aa84da723890912d5ce8f2b4f6d4103ce54dbee2
Omar Shaikh <omarasifshaikh@hotmail.com>
2016-04-09 20:00:45 -0700

    Starter Code

---
+++ src/Circle.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "Point2D.h"
+#include "CustomException.h"
+
+
+class Circle{
+};
---
+++ src/Square.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Point2D.h"
+#include "Rectangle.h"
+
+
+class Square{
+
+};
