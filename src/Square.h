598d3de79af04d35ff0f96d58eb0836ac115bd24
daluli <dalu@mail.sfsu.edu>
2016-05-22 17:44:35 +0800

    update hw6/Square.h
    Dalu Li, implement class Square

--- src/Square.h
+++ src/Square.h
@@ -3,7 +3,9 @@
 #include "Point2D.h"
 #include "Rectangle.h"
 
-
-class Square{
-
+template<class T>
+class Square : public Rectangle<T>
+{
+public:
+	Square(T x, T y, T side) : Rectangle<T>(x, y, side, side){}
 };
