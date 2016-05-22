aed626d2ca81ae44a2981ead62da19acdcd24963
daluli <dalu@mail.sfsu.edu>
2016-05-22 17:41:16 +0800

    update hw6/Circle.h
    Dalu Li, implement class Circle

--- src/Circle.h
+++ src/Circle.h
@@ -2,7 +2,40 @@
 
 #include "Point2D.h"
 #include "CustomException.h"
+#include <math.h>
 
+template<class T>
+class Circle : public Point2D<T>
+{
+public:
+	Circle(){}
+	
+	Circle(T x, T y, T r) : Point2D<T>(x, y), radius(r)
+	{
+		if(r==0) throw CustomException(22);
+	}
+	
+	void setRadius(T r)
+	{
+		if(r==0) throw CustomException(22);
+		radius = r;
+	}
+	
+	T getRadius()
+	{
+		return radius;
+	}
 
-class Circle{
+	float area() const
+	{
+		return M_PI * radius * radius;
+	}
+
+    float circumference() const
+	{
+		return 2 * M_PI * radius;
+	}
+
+private:
+	T radius;
 };
